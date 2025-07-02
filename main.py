18654f31e855d07d1213624c7b03511bf2dbd63c
usk-i <nechyshpan@mail.ru>
2025-07-02 03:59:56 +0300

    My ferstTH commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print ("Helo world")# This is a sample Python script.
 
 # Pregss Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Press Double Shift to search for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
